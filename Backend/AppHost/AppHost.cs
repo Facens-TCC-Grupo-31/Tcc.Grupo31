23a7d2957f24c6a3c449b1d82ff0ab6ef7c6b60c
pedroghiotti <pedro.ghiotti.m@gmail.com>
2026-04-16 01:07:08 -0300

    feat: added Mosquitto to apphost; added consumer worker project.

--- Backend/AppHost/AppHost.cs
+++ Backend/AppHost/AppHost.cs
@@ -1,3 +1,14 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+var mosquitto = builder
+    .AddContainer("mosquitto", "eclipse-mosquitto:alpine")
+    .WithBindMount(
+        Path.Combine(builder.Environment.ContentRootPath, "mosquitto.conf"),
+        "/mosquitto/config/mosquitto.conf")
+    .WithEndpoint(port: 1883, targetPort: 1883, scheme: "mqtt", isExternal: true);
+
+
+builder.AddProject<Projects.Consumer>("consumer");
+
+
 builder.Build().Run();
